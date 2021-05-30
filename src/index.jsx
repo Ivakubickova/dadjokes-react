4f01546364ddbf02e530a70f31c6f7185ac616a7
Iva Kubickova <ivakubickova55@gmail.com>
2021-05-30 12:33:54 +0200

    ukol

---
+++ src/index.jsx
@@ -0,0 +1,92 @@
+import React, {useState } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+
+
+
+
+  const JokerUser = () => {
+  return (
+  
+    // <div className="joke__users">
+      <div className="user-avatar">
+      <img src="/img/user01.png"></img>
+      
+      <p className="user-name">Neroxx</p>
+
+     
+    </div> 
+    // </div>
+  );
+};
+
+
+ const JokeText = () => {
+
+  return (
+    // <div className="joke__text">
+    <p>
+    The secret service isn't allowed to yell "Get down!" anymore when
+    the president is about to be attacked. Now they have to yell
+    "Donald, duck!
+  </p>
+  // </div>
+  );
+};
+
+
+
+
+
+const Likes = () => {
+
+  const [likes, setLikes] = useState(0);
+
+  const upLikes = () => {
+    setPocet(likes + 1);
+  }
+
+  const downLikes = () => {
+    setPocet(likes - 1);
+  }
+
+  return (
+    //<div className="joke__likes">
+<>
+
+      <button id="btn-up" className="btn-like btn-like--up" onClick={ upLikes }></button>
+      <button id="btn-down" className="btn-like btn-like--down"onClick={ downLikes }></button>
+
+      <span id="likes-up" className="likes-count likes-count--up" onClick={ () => setLikes(likes + 1) }>0</span>
+      <span id="likes-down" className="likes-count likes-count--down" onClick={ () => setLikes(likes - 1) }>0</span>
+
+      </>
+    //</div>
+  );
+};
+
+
+const App = () => (
+  
+  <div className="container">
+  <div className="joke">
+  <div className="joke__body">
+  <div className="joke__user">
+  <JokerUser />
+  </div>
+
+  <div className="joke__text">
+  <JokeText />
+  </div>
+  <div className="joke__likes">
+  <Likes />
+  </div>
+
+  </div>
+  </div>
+  </div>
+  
+);
+
+render(<App />, document.querySelector('#app'));
